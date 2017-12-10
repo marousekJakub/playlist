7de64703d862eafbda9e6ebdd2ae2201eb558612
Jakub Maroušek <marousej@artax.karlin.mff.cuni.cz>
2017-12-10 15:55:33 +0100

    umíme zobarzovat widgety v playlistu

--- src/app.py
+++ src/app.py
@@ -6,7 +6,18 @@ from gui import TrackInfo, RootLayout
 
 class PlaylistApp(App):
     def build(self):
-        return RootLayout(bg_path='/home/kuba/playlist2/src/party.jpg')       
+        rl = RootLayout(bg_path='/home/kuba/playlist2/src/party.jpg')
+        rl.playlist.bind(size=self.place_test_widgets)
+        return rl
+    
+    def place_test_widgets(self, *args):
+        p = self.root.playlist
+        print(p.num_rows(), p.num_cols())
+        if p.num_rows() > 0 and p.num_cols() > 0:
+            for col in range(p.num_cols()):
+                for row in range(p.num_rows()):
+                    p.place_widget(col, row, TrackInfo(track_artist="artist", track_name="name"))
+
 
 if __name__ == '__main__':
     PlaylistApp().run()
--- src/gui.py
+++ src/gui.py
@@ -11,6 +11,7 @@ from kivy.properties import StringProperty, NumericProperty
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from math import floor
 
 class TrackInfoLabel(Label):
     def __init__(self, track_info, **kwargs):
@@ -32,23 +33,66 @@ class TrackInfo(BoxLayout):
         self.width = 150
         self.height = 50
         self.size_hint = (None, None)
-        with self.canvas.before:
-            Color(rgba=(0.5, 0.5, 0.5, 1))
-            Rectangle(pos=self.pos, size=self.size)
+        self.bind(pos=self.set_background)
         
         self.add_widget(TrackInfoLabel(self, text=self.track_artist))
         self.add_widget(TrackInfoLabel(self, text=self.track_name))
+    
+    def set_background(self, *args):
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(rgba=(0.5, 0.5, 0.5, 1))
+            Rectangle(pos=self.pos, size=self.size)
 
 
 class Playlist(FloatLayout):
-    cols = NumericProperty()
-    rows = NumericProperty()
     elem_width = NumericProperty()
     elem_height = NumericProperty()
+    _cols = NumericProperty()
+    _rows = NumericProperty()
+    _cell_width = NumericProperty()
+    _cell_height = NumericProperty()
 
     def __init__(self, **kwargs):
         super(Playlist, self).__init__(**kwargs)
-        self.bind(on_size=self.clear_widgets)
+        self.bind(size=self.size_update)
+        self.widget_positions = {}
+    
+    def size_update(self, *size):
+        print("updating size")
+        self.clear_widgets()
+        self.widget_positions.clear()
+        if self.elem_width == 0 or self.elem_height == 0:
+            self._cols = self._rows = 0
+            self._cell_width = self._cell_height = 0
+        else:
+            self._cols = int(floor(self.width / self.elem_width))
+            self._rows = int(floor(self.height / self.elem_height))
+            if self._cols > 0 and self._rows > 0:
+                self._cell_width = int(floor(self.width / self._cols))
+                self._cell_height = int(floor(self.height / self._rows))
+            else:
+                self._cell_width = self._cell_height = 0
+
+    def place_widget(self, col, row, widget):
+        if not ( (0 <= col < self._cols) and (0 <= row <= self._rows) ):
+            raise Exception("Attempt to place widget at non-existent position %d, %d" % (col, row))
+        elif (col, row) in self.widget_positions:
+            raise Exception("Attemp to place widget to occupied position %d %d" % (col, row))
+        else:
+            widget.center_x = self.x + (col+0.5) * self._cell_width
+            widget.center_y = self.y + (row+0.5) * self._cell_height
+            self.add_widget(widget)
+            self.widget_positions[(col, row)] = widget
+    
+    def is_widget_placed(self, col, row):
+        return (col, row) in self.widget_positions
+
+    def num_cols(self):
+        return self._cols
+
+    def num_rows(self):
+        return self._rows
 
 
 class MenuButton(Button):
@@ -88,7 +132,7 @@ class RootLayout(BoxLayout):
         self.show_tracks = MenuButton(text=u"Správce skladeb")
         self.menu_bar = MenuBar()
         self.progress_bar = ProgressBar(max=100, value=50, size_hint_y=None, height=5)
-        self.playlist = Playlist()
+        self.playlist = Playlist(elem_width=160, elem_height=60)
         self.search = TextInput(multiline=False, size_hint_y=None)
 
         self.search.height = self.search.font_size + 15
